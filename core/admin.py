fde0d0688b7f6026f07b668a744688603ea42976
Andrii Sosunovych <sosunovych.andrii@gmail.com>
2025-05-05 19:12:01 +0300

    Add custom Admin setup for core models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,114 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from core.models import(
+    Project,
+    Position,
+    Worker,
+    TaskType,
+    Task
+)
+
+
+admin.site.unregister(Group)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    search_fields = ("name", "description")
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "position",
+        "project"
+    )
+    list_select_related = ("position", "project")
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": ("username", "password")
+            }
+        ),
+        (
+            "Personal info",
+            {
+                "fields": ("first_name", "last_name", "email")
+            }
+        ),
+        (
+            "Permissions",
+            {
+            "fields": ("is_active", "is_staff", "is_superuser"),
+            }
+        ),
+        (
+            "Important dates",
+            {
+                "fields": ("last_login", "date_joined")
+            }
+        ),
+        (
+            "Additional info",
+            {
+                "fields": ("position", "project"),
+            }
+        ),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            None,
+            {
+                "fields": ("position", "project")
+            }
+        ),
+    )
+    list_filter = (
+        "is_superuser",
+        "is_active",
+        "is_staff",
+        "position",
+        "project"
+    )
+
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "priority",
+        "deadline",
+        "is_completed",
+        "task_type",
+        "project",
+        "worker"
+    )
+    list_select_related = ("task_type", "project", "worker")
+    list_editable = ("deadline",)
+    search_fields = ("name", "description")
+    list_filter = (
+        "is_completed",
+        "deadline",
+        "priority",
+        "task_type",
+        "project",
+        "worker"
+    )
+    date_hierarchy = "deadline"
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-# from django.contrib import admin
+from django.contrib import admin
 from django.urls import path
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 
 urlpatterns = [
-    #    path('admin/', admin.site.urls),
+       path("admin/", admin.site.urls),
 ] + debug_toolbar_urls()
