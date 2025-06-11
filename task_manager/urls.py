d73fd53ccfe9ea37b420b94e3698b10e8f2de5e6
Andrii Sosunovych <sosunovych.andrii@gmail.com>
2025-06-11 13:30:44 +0300

    Configure project urls with core app and static files

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
        path("admin/", admin.site.urls),
-] + debug_toolbar_urls()
+       path("", include("core.urls", namespace="core")),
+] + debug_toolbar_urls() + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
