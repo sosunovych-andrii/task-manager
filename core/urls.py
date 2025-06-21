c0daa4e19ed9e8c4a7c7165b1a1e700cf1498cc3
Andrii Sosunovych <sosunovych.andrii@gmail.com>
2025-06-21 19:02:24 +0300

    Fix worker url names for update/delete routes

--- core/urls.py
+++ core/urls.py
@@ -12,7 +12,6 @@ from core.views import (
 )
 
 
-
 urlpatterns = [
     path("", index, name="index"),
     path("sign-up/", sign_up, name="sign-up"),
@@ -20,8 +19,8 @@ urlpatterns = [
     path("workers/", WorkerListView.as_view(), name="worker-list"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path("my-profile/", MyProfileView.as_view(), name="my-profile"),
-    path("projects/<int:pk>/update/", WorkerUpdateView.as_view(), name="worker-update"),
-    path("projects/<int:pk>/delete/", WorkerDeleteView.as_view(), name="worker-delete")
+    path("workers/<int:pk>/update/", WorkerUpdateView.as_view(), name="worker-update"),
+    path("workers/<int:pk>/delete/", WorkerDeleteView.as_view(), name="worker-delete")
 ]
 
 app_name = "core"
