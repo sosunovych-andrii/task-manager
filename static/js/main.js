a047208bee49b604f6230dc83e8ae08116ab5f60
Andrii Sosunovych <sosunovych.andrii@gmail.com>
2025-06-28 10:04:57 +0300

    Customize select2 appearance

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,20 @@ document.addEventListener("DOMContentLoaded", function () {
         dropdownAutoWidth: true,
         dropdownCssClass: "select-dropdown",
     }).on("select2:open", function () {
-        $(".select-dropdown").css("color", "black");
+        $(".select-dropdown").css(
+            "color", "black",
+        );
+    });
+    $(".select2-selection").css({
+        "height": "38px",
+        "line-height": "38px",
+        "padding-top": "4px",
+        "padding-right": "15px"
+    });
+    $(".select2-container").css({
+        "margin-right": "15px"
+    });
+    $(".select2-selection__arrow").css({
+        "height": "38px"
     });
 });
