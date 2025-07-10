337219de12403fc4ac9ec2222b26780966c95dc2
Andrii Sosunovych <sosunovych.andrii@gmail.com>
2025-07-10 12:54:07 +0300

    Add additional allowed host for deployment

--- task_manager/settings/prod.py
+++ task_manager/settings/prod.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["task-manager-pxq2.onrender.com", "127.0.0.1", "localhost"]
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
